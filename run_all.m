4074e34db970799f63d2ff54f4948c1a6e3e5159
seongyeol-code <silgil8119@gmail.com>
2026-09-22 16:07:34 +0900

    Add script to run all KNN MATLAB exercises
    This script sequentially runs KNN MATLAB exercises and prompts the user after each one.

---
+++ run_all.m
@@ -0,0 +1,8 @@
+%% KNN MATLAB 실습 전체 실행 (ex1 ~ ex5)
+% 각 스크립트가 clear/close all을 호출하므로 변수 없이 순서대로 호출한다.
+% 각 실습이 끝나면 그림을 확인한 뒤 아무 키나 눌러 다음 실습으로 넘어간다.
+ex1_dataload_visualization;  disp('ex1 완료 - 아무 키나 누르면 ex2 실행'); pause;
+ex2_knn_using_matlab;        disp('ex2 완료 - 아무 키나 누르면 ex3 실행'); pause;
+ex3_knn_my_style;            disp('ex3 완료 - 아무 키나 누르면 ex4 실행'); pause;
+ex4_knn_generalization;      disp('ex4 완료 - 아무 키나 누르면 ex5 실행'); pause;
+ex5_knn_space;               disp('ex5 완료');
